2c26f06e47cc4aa5c9e8938a71915f15cfbda74f
Pato <infotvorai@gmail.com>
2026-03-23 17:05:50 +0100

    Make trainer dashboard full width by removing shell wrapper

--- src/app/(trainer)/layout.tsx
+++ src/app/(trainer)/layout.tsx
@@ -1,6 +1,3 @@
-import { DashboardShell } from "@/components/DashboardShell";
-import { TrainerSidebar } from "@/components/Sidebar";
-
 export default function TrainerLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardShell sidebar={<TrainerSidebar />}>{children}</DashboardShell>;
+  return <>{children}</>;
 }
